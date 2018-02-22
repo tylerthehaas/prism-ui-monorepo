99a04668408de8b3f49c37a9b78c0e7e4f21fcf0
Nhat Nguyen <ndnhat@hotmail.com>
2018-02-22 14:29:44 -0700

    Serve fonts with CORS

--- packages/deprecated-prism-ui/server/index.js
+++ packages/deprecated-prism-ui/server/index.js
@@ -1,17 +1,20 @@
 const path = require('path');
 const util = require('util');
+const cors = require('cors');
 const express = require('express');
 const compress = require('compression');
 const style = require('./style');
 
 const server = express();
 const port = process.env.PORT || 3000;
-const staticPath = path.join(__dirname, '..', 'docs');
+const docsPath = path.join(__dirname, '..', 'docs');
+const fontsPath = path.join(__dirname, '..', 'fonts');
 
 server
   .use(compress({threshold: 0}))
   .use('/css', style)
-  .use('/docs', express.static(staticPath));
+  .use('/fonts', cors(), express.static(fontsPath))
+  .use('/docs', express.static(docsPath));
 
 server.listen(port, err => {
   if (err) {
