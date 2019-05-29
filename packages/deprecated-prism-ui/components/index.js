3bd93beeb27dd0d15fd144284cad8460eba03f0a
Jeff Verhaaren <jeff-v@users.noreply.github.com>
2019-05-29 14:13:15 -0600

    CCLOUD-211 fixed another mistake (#88)
    * CCLOUD-211 fixed another mixtake"
    
    * CCLOUD-211 fixed a dumb webpack error

--- packages/deprecated-prism-ui/components/index.js
+++ packages/deprecated-prism-ui/components/index.js
@@ -1,18 +1,38 @@
-export const Button = require('./button/Button');
-export const Input = require('./input/Input');
-export const Radio = require('./radio/Radio');
-export const Alert = require('./alert/Alert');
-export const Pagination = require('./pagination/Pagination.tsx');
-export const Nav = require('./nav/Nav.tsx');
-export const List = require('./list/List.tsx');
-export const Toggle = require('./toggle/Toggle.tsx');
-export const Table = require('./table/Table.tsx');
-export const Card = require('./card/Card.tsx');
-export const Hr = require('./hr/Hr.tsx');
-export const Rollover = require('./rollover/Rollover.tsx');
-export const Chip = require('./chip/Chip.tsx');
-export const Modal = require('./modal/Modal.tsx');
-export const Avatar = require('./avatar/Avatar');
+const assert = require('assert');
+
+const ButtonSource = require('./button/Button');
+const InputSource = require('./input/Input');
+const RadioSource = require('./radio/Radio');
+const AlertSource = require('./alert/Alert');
+const PaginationSource = require('./pagination/Pagination');
+const NavSource = require('./nav/Nav');
+const ListSource = require('./list/List');
+const ToggleSource = require('./toggle/Toggle');
+const TableSource = require('./table/Table');
+const CardSource = require('./card/Card');
+const HrSource = require('./hr/Hr');
+const RolloverSource = require('./rollover/Rollover');
+const ChipSource = require('./chip/Chip');
+const ModalSource = require('./modal/Modal');
+const AvatarSource = require('./avatar/Avatar');
+const IconSource = require('./icon/Icon');
+
+export const Button = ButtonSource.default;
+export const Input = InputSource.default;
+export const Radio = RadioSource.default;
+export const Alert = AlertSource.default;
+export const Pagination = PaginationSource.default;
+export const Nav = NavSource.default;
+export const List = ListSource.default;
+export const Toggle = ToggleSource.default;
+export const Card = CardSource.default;
+export const Hr = HrSource.default;
+export const Rollover = RolloverSource.default;
+export const Chip = ChipSource.default;
+export const Modal = ModalSource.default;
+export const Avatar = AvatarSource.default;
+export const Table = TableSource.default;
+export const Icon = IconSource.default;
 
 const Prism = {
   Button,
